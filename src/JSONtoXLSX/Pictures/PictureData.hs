d9fc4616492d06393598c516b206f49aa4b1396c
stla <laurent_step@yahoo.fr>
2017-06-17 00:14:31 +0200

    Pictures Tests

--- src/JSONtoXLSX/Pictures/PictureData.hs
+++ src/JSONtoXLSX/Pictures/PictureData.hs
@@ -1,11 +1,9 @@
-{-# LANGUAGE DeriveAnyClass    #-}
-{-# LANGUAGE DeriveGeneric     #-}
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveGeneric  #-}
 
 module JSONtoXLSX.Pictures.PictureData
   where
-import           Data.Aeson
-import           Data.ByteString.Lazy.UTF8 (fromString)
+import           Data.Aeson   (FromJSON)
 import           GHC.Generics
 
 data PictureData = PictureData
@@ -17,16 +15,3 @@ data PictureData = PictureData
                      height :: Int
                     }
                    deriving (Show, Generic, FromJSON, Eq)
-
-jj = "{\"file\":\"image.png\",\"left\":1,\"top\":1,\"width\":100,\"height\":100}"
-test2 = decode (fromString jj) :: Maybe PictureData
-
--- two images
-jjj = "[{\"file\":\"image.png\",\"left\":1,\"top\":1,\"width\":100,\"height\":100},{\"file\":\"image.png\",\"left\":1,\"top\":1,\"width\":100,\"height\":100}]"
-ttest2 = decode (fromString jjj) :: Maybe [PictureData]
-
--- idea:
--- writeXlsx data images outfile
--- data = Maybe String (json string) - idem already done
--- images = Maybe String (json string)
---  {Sheet1: jjj, Sheet2: ...}
