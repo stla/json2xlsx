6128d8bebd8ce1236fbd2de3ba0bdc778e9f707c
stla <laurent_step@yahoo.fr>
2017-06-17 00:37:33 +0200

    cleaning

--- src/JSONtoXLSX/JSONtoCellMap.hs
+++ src/JSONtoXLSX/JSONtoCellMap.hs
@@ -1,26 +1,16 @@
--- {-# LANGUAGE OverloadedStrings #-}
 module JSONtoXLSX.JSONtoCellMap
   where
 import           Codec.Xlsx
 import           Codec.Xlsx.Formatted
--- import           Data.Aeson               (decode)
 import           JSONtoXLSX.JSONtoCellMap.Conversions
 import           JSONtoXLSX.JSONtoCellMap.Types
--- import qualified Data.HashMap.Lazy         as DHM
--- import           Data.Map.Lazy             (Map)
--- import qualified Data.Map.Lazy             as DM
 import           Control.Lens                         (set)
--- import           Data.ByteString.Lazy.UTF8 (fromString)
 import           Data.Aeson                           (decode)
 import           Data.ByteString.Lazy.UTF8            (fromString)
-import qualified Data.HashMap.Lazy                    as DHM
-import qualified Data.Map.Lazy                        as DM
+import qualified Data.HashMap.Lazy                    as HM
+import qualified Data.Map.Lazy                        as M
 import           Data.Maybe                           (fromJust, fromMaybe)
 
-
--- for testing
-jj :: String
-jj = "{\"A1\":{\"value\":2,\"format\":{\"numberFormat\":\"Nf2Decimal\",\"font\":{\"bold\":true}}},\"B2\":{\"value\":1000,\"format\":{\"numberFormat\":\"yyyy-mm-dd;@\"}},\"A3\":{\"value\":\"abc\",\"format\":{\"font\":{\"family\":\"Script\",\"name\":\"Courier\"}}}}"
 -- {
 --   "A1": {
 --     "value": 2,
@@ -55,12 +45,12 @@ jj = "{\"A1\":{\"value\":2,\"format\":{\"numberFormat\":\"Nf2Decimal\",\"font\":
 -- cellA1 = fromJust $ DHM.lookup "A1" x
 -- -- Convert to Map (Int, Int) SimpleCell
 -- y :: Map Text SimpleCell
--- y = DM.fromList $ DHL.toList x
+-- y = M.fromList $ DHL.toList x
 -- z :: Map (Int, Int) SimpleCell
--- z = DM.mapKeys (\x -> fromJust $ fromSingleCellRef CellRef {unCellRef = x}) y
+-- z = M.mapKeys (\x -> fromJust $ fromSingleCellRef CellRef {unCellRef = x}) y
 -- -- Convert to FormattedCellMap
 -- formattedcellmap :: Map (Int, Int) FormattedCell
--- formattedcellmap = DM.map simpleCellToFormattedCell z
+-- formattedcellmap = M.map simpleCellToFormattedCell z
 
 simpleCellToFormattedCell :: SimpleCell -> FormattedCell
 simpleCellToFormattedCell scell =
@@ -79,9 +69,9 @@ simpleCellToFormattedCell scell =
 
 simpleCellMapToFormattedCellMap :: SimpleCellMap -> FormattedCellMap
 simpleCellMapToFormattedCellMap simplecellmap =
-  DM.map simpleCellToFormattedCell $
-    DM.mapKeys (\c -> fromJust $ fromSingleCellRef CellRef {unCellRef = c}) $
-      DM.fromList $ DHM.toList simplecellmap
+  M.map simpleCellToFormattedCell $
+    M.mapKeys (\c -> fromJust $ fromSingleCellRef CellRef {unCellRef = c}) $
+      M.fromList $ HM.toList simplecellmap
 
 jsonToFormattedCellMap :: String -> FormattedCellMap
 jsonToFormattedCellMap jsonString =
