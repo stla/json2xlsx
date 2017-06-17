a9a09fca03a2a0876d3efee8aafc0dd01897364e
stla <laurent_step@yahoo.fr>
2017-06-17 19:35:59 +0200

    update exe

--- README.md
+++ README.md
@@ -3,3 +3,15 @@
 ```
 writexlsx {\"Sheet1\":{\"A1\":{\"value\":2,\"format\":{\"numberFormat\":\"2Decimal\",\"font\":{\"bold\":true}}},\"B2\":{\"value\":1000,\"format\":{\"numberFormat\":\"yyyy-mm-dd;@\"}},\"A3\":{\"value\":\"abc\",\"format\":{\"font\":{\"name\":\"Courier\",\"color\":\"red\"}}}}} -o output.xlsx
 ```
+
+```
+Set jsonCells={\"Sheet1\":{\"A1\":{\"value\":2,\"format\":{\"numberFormat\":\"2Decimal\",\"font\":{\"bold\":true}}},\"B2\":{\"value\":1000,\"format\":{\"numberFormat\":\"yyyy-mm-dd;@\"}},\"A3\":{\"value\":\"abc\",\"format\":{\"font\":{\"name\":\"Courier\",\"color\":\"red\"}}}}}
+writexlsx -c %jsonCells% -o output.xlsx
+```
+
+-   Include images:
+
+```
+Set jsonImages={\"Sheet1\":[{\"file\":\"image.png\",\"left\":3,\"top\":3,\"width\":200,\"height\":300}],\"Sheet2\":[{\"file\":\"image2.png\",\"left\":2,\"top\":3,\"width\":200,\"height\":300}]}
+writexlsx -c %jsonCells% -i %jsonImages% -o output.xlsx
+```
--- src-commandline/Main.hs
+++ src-commandline/Main.hs
@@ -3,21 +3,43 @@ module Main
 import           Data.Monoid                   ((<>))
 -- import qualified Data.Text                     as T
 import           Options.Applicative
-import           WriteXLSX
+import           JSONtoXLSX
+import qualified Data.ByteString.Lazy       as BL
+-- import qualified Data.ByteString.Lazy.Char8 as L
+import           Data.ByteString.Lazy.UTF8  (toString)
+import           System.Directory           (doesFileExist)
+import Data.Maybe
+
+getJSON :: String -> IO String
+getJSON json = do
+  isFile <- doesFileExist json
+  if isFile then
+    BL.readFile json >>= (return . toString)
+  else return json
 
 data Arguments = Arguments
-  { json :: String
-  , file :: FilePath }
+  {
+    jsonCells  :: Maybe String
+  , jsonImages :: Maybe String
+  , file       :: FilePath
+  }
 
 writeXLSX :: Arguments -> IO()
-writeXLSX (Arguments json file) =
-  writeXlsx json file
+writeXLSX (Arguments jsonCells jsonImages file) = do
+  let json1 = fromMaybe "{}" jsonCells
+      json2 = fromMaybe "{}" jsonImages
+  writeXlsx3 json1 json2 file
 
 run :: Parser Arguments
 run = Arguments
-     <$> strArgument
-          ( metavar "JSON"
-         <> help "JSON string" )
+     <$> optional (strOption
+           ( long "cells"
+          <> short 'c'
+          <> help "JSON string for cells" ))
+     <*> optional (strOption
+           ( long "images"
+          <> short 'i'
+          <> help "JSON string for images" ))
      <*> strOption
            ( long "output"
           <> short 'o'
@@ -29,6 +51,6 @@ main = execParser opts >>= writeXLSX
  where
    opts = info (helper <*> run)
      ( fullDesc
-    <> progDesc "Write a XLSX sheet from a JSON string"
+    <> progDesc "Write a XLSX worksheet from JSON strings"
     <> header "writexlsx"
     <> footer "Author: Stéphane Laurent" )
